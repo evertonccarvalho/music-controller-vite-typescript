bb7de27cf99a7e85485e4a9f5f41b63a08315a89
Éverton Carvalho <evertonsnake@gmail.com>
2024-04-19 18:49:26 -0300

    feat: react and taiwlind css

---
+++ src/app.tsx
@@ -0,0 +1,3 @@
+export default function App() {
+  return <div className="bg-red-700"><p className="bg-red-50">hello</p></div>
+}
---
+++ src/index.tsx
@@ -0,0 +1,5 @@
+import { createRoot } from "react-dom/client"
+import App from "./app"
+import "./main.css"
+const root = createRoot(document.body)
+root.render(<App />)
